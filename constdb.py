99359c200d50315309edd560489886fc79bbc1e4
Ethan Steinberg <ethan.steinberg@gmail.com>
2018-04-17 16:50:53 -0700

    Support no mmap

--- constdb.py
+++ constdb.py
@@ -1,11 +1,15 @@
 import struct
 import mmap
+import os
 
 def create(name):
     return Writer(name)
 
-def read(name):
-    return Reader(name)
+def read(name, keys_to_read=None, mmap=True):
+    if mmap:
+        return MmapReader(name, keys_to_read=keys_to_read)
+    else:
+        return FseekReader(name, keys_to_read=keys_to_read)
 
 table_offset_format = '<q'
 table_entry_format = '<iqqq'
@@ -51,8 +55,68 @@ class Writer:
     def __exit__(self, *args):
         self.close()
 
-class Reader:
-    def __init__(self, name):
+class FseekReader:
+    def __init__(self, name, keys_to_read=None):
+        self.file = open(name, 'rb')
+
+        size_of_table_offset = struct.calcsize(table_offset_format)
+        size_of_table_entry = struct.calcsize(table_entry_format)
+
+        self.file.seek(-size_of_table_offset, os.SEEK_END)
+
+        location_of_offset = self.file.tell()
+
+        table_offset, = struct.unpack(table_offset_format, self.file.read(size_of_table_offset))
+
+        self.offsets = {}
+
+        self.file.seek(table_offset)
+
+        current_location = table_offset
+        while current_location < location_of_offset:
+            key_type, begin, end, key_val = struct.unpack(table_entry_format, self.file.read(size_of_table_entry))
+            current_location += size_of_table_entry
+
+            if key_type == INT_KEY_TYPE:
+                key = key_val
+            elif key_type == STR_KEY_TYPE:
+                key = self.file.read(key_val).decode('utf-8')
+                current_location += key_val
+
+            if keys_to_read is not None and key not in keys_to_read:
+                continue
+
+            self.offsets[key] = (begin, end)
+
+        all_items = list(self.offsets.items())
+        all_items.sort(key=lambda a:a[1][0])
+
+        self.computed_keys = [a[0] for a in all_items]
+
+    def get(self, key):
+        if key not in self.offsets:
+            return None
+        else:
+            begin, end =  self.offsets[key]
+
+            self.file.seek(begin)
+
+            return self.file.read(end - begin)
+
+    def keys(self):
+        return self.computed_keys
+
+    def close(self):
+        self.file.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+class MmapReader:
+    def __init__(self, name, keys_to_read=None):
         with open(name, 'rb') as file:
             self.map = mmap.mmap(file.fileno(), 0, prot=mmap.PROT_READ)
 
@@ -75,6 +139,9 @@ class Reader:
             elif key_type == STR_KEY_TYPE:
                 key = self.map[current_location:current_location+key_val].decode('utf-8')
                 current_location += key_val
+
+            if keys_to_read is not None and key not in keys_to_read:
+                continue
 
             self.offsets[key] = (begin, end)
 
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import constdb
 import tempfile
 import os.path
 
-def test_main():
+def test_mmap():
     with tempfile.TemporaryDirectory() as temp_dir:
         file_name = os.path.join(temp_dir, 'test_file')
         with constdb.create(file_name) as db:
@@ -20,3 +20,41 @@ def test_main():
             assert db.get(6) == b'26'
 
             assert db.keys() == [-2, 3, -1, 'Hello world', 6]
+
+        with constdb.read(file_name, keys_to_read={'Hello world', 3}) as db:
+            assert db.get(-2) is None
+            assert db.get(-1) is None
+            assert db.get(3) == b'23'
+            assert db.get('Hello world') ==  b'67'
+            assert db.get(6) is None
+
+            assert db.keys() == [3, 'Hello world']
+
+
+def test_fseek():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        file_name = os.path.join(temp_dir, 'test_file')
+        with constdb.create(file_name) as db:
+            db.add(-2, b'7564')
+            db.add(3, b'23')
+            db.add(-1, b'66')
+            db.add('Hello world', b'67')
+            db.add(6, b'26')
+
+        with constdb.read(file_name, mmap=False) as db:
+            assert db.get(-2) == b'7564'
+            assert db.get(-1) == b'66'
+            assert db.get(3) == b'23'
+            assert db.get('Hello world') ==  b'67'
+            assert db.get(6) == b'26'
+
+            assert db.keys() == [-2, 3, -1, 'Hello world', 6]
+
+        with constdb.read(file_name, keys_to_read={'Hello world', 3}, mmap=False) as db:
+            assert db.get(-2) is None
+            assert db.get(-1) is None
+            assert db.get(3) == b'23'
+            assert db.get('Hello world') ==  b'67'
+            assert db.get(6) is None
+
+            assert db.keys() == [3, 'Hello world']
